04999eb3cdd30d58bd3f357c34ae54af4bfb90ab
yveso <yveso@users.noreply.github.com>
2019-12-10 13:43:31 +0100

    They can talk to each other

--- containers/next-js/pages/index.js
+++ containers/next-js/pages/index.js
@@ -1,5 +1,28 @@
-function Index() {
-  return <div>Hello Next</div>;
+import fetch from "isomorphic-unfetch";
+
+function Index(props) {
+  console.log(props);
+  return (
+    <div>
+      <h1>Users</h1>
+      <ul>
+        {props.users.map(user => (
+          <li key={user.id}>{user.username}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
+
+Index.getInitialProps = async function() {
+  const res = await fetch("http://localhost:5001/users");
+  const data = await res.json();
+
+  console.log(data);
+
+  return {
+    users: data.data.users,
+  };
+};
 
 export default Index;
